5aa4a5c855d0da90174f7138500a4ebdeeb95487
Tony Gaeta <tony.gaeta@gmail.com>
2015-03-24 23:56:27 -0400

    refactored code, fixed two tests, applied ruby style guide to code

--- about_control_statements.rb
+++ about_control_statements.rb
@@ -1,7 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutControlStatements < Neo::Koan
-
   def test_if_then_else_statements
     if true
       result = :true_value
@@ -84,28 +83,28 @@ class AboutControlStatements < Neo::Koan
     i = 1
     result = 1
     while i <= 10
-      result = result * i
+      result *= i
       i += 1
     end
-    assert_equal 3628800, result
+    assert_equal 3_628_800, result
   end
 
   def test_break_statement
     i = 1
     result = 1
-    while true
+    loop do
       break unless i <= 10
-      result = result * i
+      result *= i
       i += 1
     end
-    assert_equal 3628800, result
+    assert_equal 3_628_800, result
   end
 
   def test_break_statement_returns_values
     i = 1
     result = while i <= 10
-      break i if i % 2 == 0
-      i += 1
+               break i if i.even?
+               i += 1
     end
 
     assert_equal 2, result
@@ -116,19 +115,19 @@ class AboutControlStatements < Neo::Koan
     result = []
     while i < 10
       i += 1
-      next if (i % 2) == 0
+      next if i.even?
       result << i
     end
     assert_equal [1, 3, 5, 7, 9], result
   end
 
   def test_for_statement
-    array = ["fish", "and", "chips"]
+    array = %w(fish and chips)
     result = []
     for item in array
       result << item.upcase
     end
-    assert_equal ["FISH", "AND", "CHIPS"], result
+    assert_equal %w(FISH AND CHIPS), result
   end
 
   def test_times_statement
@@ -138,5 +137,4 @@ class AboutControlStatements < Neo::Koan
     end
     assert_equal 10, sum
   end
-
 end
